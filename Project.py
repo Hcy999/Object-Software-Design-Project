02acb5036fe045849c7f9b58cbafc0e86277edf2
Hcy999 <m568912049@gmail.com>
2024-01-28 19:24:50 -0500

    update Timer Class

--- Project.py
+++ Project.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import time
 
 class Road:
     def __init__(self, name, length):
@@ -208,3 +209,16 @@ class GUI:
 class Timer:
      def __init__(self):
         self.gui = None  # GUI
+        self.elapsed_time = 0
+        self.start_time = None
+
+     def start(self):
+        """Start Timer"""
+        if self.start_time is None:
+            self.start_time = time.time()
+
+     def stop(self):
+        """Stop Timer"""
+        if self.start_time is not None:
+            self.elapsed_time += time.time() - self.start_time
+            self.start_time = None
